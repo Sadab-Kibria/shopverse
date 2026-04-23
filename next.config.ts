d58873fe75364a14dacbf5995b26d36ebbb3584a
Sadab Kibria <sadabkibria123@gmail.com>
2026-04-24 03:08:23 +0600

    2nd commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   reactStrictMode: true,
+    eslint: {
+    // !! WARN !! This allows production builds to successfully complete
+    // even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["res.cloudinary.com","lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
